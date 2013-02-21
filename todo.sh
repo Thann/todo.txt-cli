adb5fc59dca169203268bcf78cd3a00c88b93d26
Jon Knapp <jon.knapp@rightscale.com>
2013-02-21 11:02:28 -0800

    added 'done' subtasks

--- todo.sh
+++ todo.sh
@@ -884,7 +884,8 @@ _format()
             s/'"${HIDE_CONTEXTS_SUBSTITUTION:-^}"'//g
             s/'"${HIDE_CUSTOM_SUBSTITUTION:-^}"'//g
           '''                                                   \
-	| sed "s/↳/\\n\t↳ /g"                                   \
+	| sed "s/↳x[^↳]*//g"                                    \
+        | sed "s/↳/\\n\t↳/g"                                    \
         | eval ${TODOTXT_FINAL_FILTER}                          \
     )
     [ "$filtered_items" ] && echo "$filtered_items"
